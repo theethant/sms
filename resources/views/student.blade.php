47f4ebe1dcd42d36ee5df182d323294184c94863
theethant <theethantmaung1418@gmail.com>
2022-06-30 21:29:19 +0630

    new

--- resources/views/student.blade.php
+++ resources/views/student.blade.php
@@ -119,8 +119,8 @@
         </div>
     @endif
 
-    <footer>
-        <span>CopyRight &copy;</span><span>Developed by Thee Thant</span>
+    <footer class="py-3 d-flex justify-content-center">
+        <span>CopyRight &copy;, All right reserve.</span><span>Developed by Thee Thant</span>
     </footer>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
